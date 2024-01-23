e32d547d7e8ba4369ff5c8026190d55d4e1322d4
Den-ie <dpokosov@gmail.com>
2024-01-23 18:46:42 +0300

    gig

--- Kosov1/MainPage.xaml.cs
+++ Kosov1/MainPage.xaml.cs
@@ -10,15 +10,20 @@ namespace Kosov1
         {
             InitializeComponent();
             disciplines = new List<Discipline>();
-            disciplines.Add(new Discipline { Name = "Математика ", Teacher = "Кавасаки В.О.", ClassNumber = 44, HoursAmount = 64});
+            disciplines.Add(new Discipline { Name = "Математика ", Teacher = "Кавасаки В.О.", ClassNumber = 44, HoursAmount = 64 });
             disciplines.Add(new Discipline { Name = "Физика ", Teacher = "Стрипер Г.П.", ClassNumber = 12, HoursAmount = 32 });
             disciplines.Add(new Discipline { Name = "Физ-ра ", Teacher = "Крико А.А.", ClassNumber = 0, HoursAmount = 16 });
             ItemsList.ItemsSource = disciplines;
         }
 
-        private void SelIt(object sender, SelectedItemChangedEventArgs e)
+        private void SelectIt(object sender, SelectedItemChangedEventArgs e)
         {
-            
+            Discipline gig = new Discipline();
+            if (e.SelectedItem != null)
+            {
+                gig = e.SelectedItem as Discipline;
+                SelectedText.Text = gig.Name.ToString();
+            }
         }
     }
 }
